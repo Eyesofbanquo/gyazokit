515e7807142aa695cfc2d9ed026b73e90e1d1695
Markim Shaw <ms79723@gmail.com>
2020-05-03 21:37:23 -0500

    Initial Setup
    Initial Project Setup involving .gitignore and removing storyboards.
    
    This project will also be using Core Data

---
+++ gyazokit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  gyazokit
+//
+//  Created by Markim Shaw on 5/3/20.
+//  Copyright © 2020 Markim Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .green
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
